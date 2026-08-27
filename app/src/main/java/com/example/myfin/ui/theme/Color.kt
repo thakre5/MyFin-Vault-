a3097484936d9013b9f849faf51aad0ea407640d
thakre5 <sushantthakre5@gmail.com>
2026-08-27 08:47:48 +0530

    Create Color.kt

---
+++ app/src/main/java/com/example/myfin/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.example.myfin.ui.theme
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+val CanvasLight = Color(0xFFF7F8FA)
+val CardWhite = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF14171A)
+val TextMuted = Color(0xFF7A869A)
+val BorderLight = Color(0xFFE2E8F0)
+
+val AccentPurple = Color(0xFF6C5CE7)
+val AccentPurpleDark = Color(0xFF5A4BD1)
+val AccentPurpleLight = Color(0xFFECE9FF)
+
+val SoftGreen = Color(0xFF00B894)
+val SoftGreenLight = Color(0xFFE6F8F4)
+
+val SoftRed = Color(0xFFFF5252)
+val SoftRedLight = Color(0xFFFFEBEB)
+
+val SoftTeal = Color(0xFF0984E3)
+val SoftTealLight = Color(0xFFE5F2FD)
+
+val SoftAmber = Color(0xFFF39C12)
+val SoftAmberLight = Color(0xFFFEF5E7)
+
+val HeroCardGradient = Brush.verticalGradient(
+    colors = listOf(
+        Color(0xFFFFFFFF),
+        Color(0xFFF8F9FE)
+    )
+)
