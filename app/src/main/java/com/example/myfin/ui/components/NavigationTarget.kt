22ee4301a5d7ae4d238b786870f1603b9370f79c
thakre5 <sushantthakre5@gmail.com>
2026-08-27 08:45:01 +0530

    Create NavigationTarget.kt

---
+++ app/src/main/java/com/example/myfin/ui/components/NavigationTarget.kt
@@ -0,0 +1,12 @@
+package com.example.myfin.ui.components
+
+enum class NavigationTarget {
+    MONTHLY_VIEW,
+    YEARLY_VIEW,
+    BUDGET_PLANNER,
+    DATA_SET,
+    VAULT_ACCOUNTS,
+    REPORTS_ANALYTICS,
+    SETTINGS,
+    USER_GUIDE
+}
