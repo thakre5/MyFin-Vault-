8906c6b6eb2f1ac618f464b95b93433dba4dee48
thakre5 <sushantthakre5@gmail.com>
2026-08-27 08:45:51 +0530

    Create SettingsActiveSheet.kt

---
+++ app/src/main/java/com/example/myfin/ui/components/SettingsActiveSheet.kt
@@ -0,0 +1,11 @@
+package com.example.myfin.ui.components
+
+enum class SettingsActiveSheet {
+    NONE,
+    PERSONAL_INFO,
+    PROFILE,
+    SECURITY,
+    VAULT_ACCOUNTS,
+    BACKUP,
+    REMINDERS
+}
