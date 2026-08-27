c48185da8756065ddf47c1168daf6c3a8e68835b
thakre5 <sushantthakre5@gmail.com>
2026-08-27 08:28:28 +0530

    Create DataModels.kt

---
+++ app/src/main/java/com/example/myfin/data/DataModels.kt
@@ -0,0 +1,22 @@
+package com.example.myfin.data
+
+data class AccountBalanceResult(
+    val accountName: String,
+    val startingBalance: Double,
+    val accountType: String,
+    val sortOrder: Int,
+    val currentBalance: Double
+)
+
+data class MonthlySummary(
+    val month: Int,
+    val totalActualIncome: Double = 0.0,
+    val totalActualExpense: Double = 0.0,
+    val totalAsset: Double = 0.0
+)
+
+data class YearlyCategoryRollup(
+    val category: String,
+    val type: TransactionType,
+    val totalActualAmount: Double
+)
