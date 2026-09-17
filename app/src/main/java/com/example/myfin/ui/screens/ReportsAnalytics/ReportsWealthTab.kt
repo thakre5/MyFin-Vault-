49a3bc135253b34e2fc1c498ba4f2abd05142112
thakre5 <sushantthakre5@gmail.com>
2026-09-17 21:15:26 +0530

    Update ReportsWealthTab.kt

--- app/src/main/java/com/example/myfin/ui/screens/ReportsAnalytics/ReportsWealthTab.kt
+++ app/src/main/java/com/example/myfin/ui/screens/ReportsAnalytics/ReportsWealthTab.kt
@@ -2,6 +2,7 @@ package com.example.myfin.ui.screens
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
